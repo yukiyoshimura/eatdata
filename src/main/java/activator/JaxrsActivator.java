7f95da22068785863cc9ce3ecd987252490de6a8
yukiyoshimura <yuukiyy0234@gmail.com>
2016-05-09 00:45:52 +0900

    change servlet mapping path for using tomcat as a webServer

--- src/main/java/activator/JaxrsActivator.java
+++ src/main/java/activator/JaxrsActivator.java
@@ -3,7 +3,7 @@ package activator;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("")
+@ApplicationPath("api")
 public class JaxrsActivator extends Application {
 	
 }
