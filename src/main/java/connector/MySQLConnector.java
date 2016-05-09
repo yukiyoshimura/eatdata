3f66b420de31b24ba995dcb0095217c4d222e278
yukiyoshimura <yuukiyy0234@gmail.com>
2016-05-10 01:04:07 +0900

    db接続情報はpropertiesファイルに持たせるように変更

--- src/main/java/connector/MySQLConnector.java
+++ src/main/java/connector/MySQLConnector.java
@@ -2,24 +2,37 @@ package connector;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 import com.mysql.jdbc.Connection;
 public class MySQLConnector {
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         Connection con = null;
-            // MySQLに接続
-				try {
-					Class.forName("com.mysql.jdbc.Driver");
-					
-					System.out.println("Connetcting.....");
-		            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/eatdata", "root", "");
-		            System.out.println("MySQLに接続できました。");
-				} catch (SQLException e) {
-					System.out.println("Connector Error MySQL接続に失敗しました。");
-					e.printStackTrace();
-					throw new SQLException();
-				}
-	            return con;
+        
+		//propertiesファイル読み込み
+		ResourceBundle bundle = ResourceBundle.getBundle("dbinfo");
+		
+		String host = bundle.getString("host");
+		String port = bundle.getString("port");
+		String user = bundle.getString("user");
+		String dbname = bundle.getString("dbname");
+		String password = bundle.getString("password");
+		String url = "jdbc:mysql://" + host + ":" + port + "/" + dbname;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			System.out.println("Connetcting....." + url);
+	        // MySQLに接続
+            con = (Connection) DriverManager.getConnection(url, user, password);
+///            con = (Connection) DriverManager.getConnection("jdbc:mysql://ja-cdbr-azure-east-a.cloudapp.net:3306/eatdata", "b3d674e06c3e5b", "bb28b0c7");
+            System.out.println("MySQLに接続できました。");
+		} catch (SQLException e) {
+			System.out.println("Connector Error MySQL接続に失敗しました。");
+			e.printStackTrace();
+			throw new SQLException();
+		}
+        return con;
 
     }
     
